f16b5989cefa05059d69d2b0b64a5c6737d2efe4
pkann <pkann@kth.se>
2025-12-03 14:45:27 +0100

    + simple file reader

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,31 @@
 #include <string>
 #include <iostream>
+#include <fstream>
 #include <Eigen/Dense>
 #include <CLI11.hpp>
 #include "arnoldi.h"
 
 
-void read_input_file(std::string filename) {
-    // TODO: everything
+Eigen::MatrixXd read_input_file(const std::string& filename) {
+    std::ifstream infile(filename);
+    if (!infile.is_open()) {
+        throw std::runtime_error("Could not open file " + filename);
+    }infile.is_open();
 
-    return;
+    int rows, cols;
+    infile >> rows >> cols;
+
+    Eigen::MatrixXd A(rows, cols);
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            double value;
+            infile >> value;
+            A(i, j) = value;
+        }
+    }
+
+    return A;
 }
 
 
@@ -21,13 +38,18 @@ int main(int argc, char **argv) {
 
     CLI11_PARSE(app, argc, argv);
 
-    Eigen::MatrixXd A(4, 4);
+    Eigen::MatrixXd A;
     if (input_file.empty()) {
+        A.resize(4,4);
         A << 1, 2, 0, 0,
              2, 3, 1, 0,
              0, 1, 2, 1,
              0, 0, 1, 4;
         std::cout << "Example Eigen matrix:\n" << A << std::endl;
+    }
+    else {
+        A = read_input_file(input_file);
+        std::cout << "Eigen matrix:\n" << A << std::endl;
     }
 
     auto test = arnoldi::solve(A, 3);
