9e428caad9ee13d4cd012cf82ea79d9e3d7b10f8
signestj <stjernstoftsigne@gmail.com>
2025-12-11 12:00:08 +0100

    Remove some comments

--- src/lanczos.h
+++ src/lanczos.h
@@ -16,8 +16,8 @@ namespace lanczos {
     template <typename TMatrix>
     LanczosResult solve(
         const TMatrix& U,
-        const Eigen::VectorXd& k1,  // k_1
-        const Eigen::VectorXd& q1,  // q_1  (stored as column; represents row q_1^T)
+        const Eigen::VectorXd& k1,
+        const Eigen::VectorXd& q1,
         const int m
     ) {
 
@@ -33,11 +33,7 @@ namespace lanczos {
         k[0] = k1;
         q[0] = q1;
 
-        // α_1,...,α_m  → alpha(0,...,m-1)
         Eigen::VectorXd alpha = Eigen::VectorXd::Zero(m);
-
-        // β_1,...,β_{m-1} → beta(0..m-2)
-        // β_0 is never stored, it is 0 in the recurrence.
         Eigen::VectorXd beta  = Eigen::VectorXd::Zero(std::max(0, m - 1));
 
         // Computing α_1
@@ -46,18 +42,12 @@ namespace lanczos {
         assert(std::abs(denom) > 0.0000001);     // sanity check for debugging
         alpha(0) = num / denom;             // α_1
 
-        // β_0 ← 0 (Don't need to store)
-
         // Iteration
         for (int i = 2; i <= m; ++i) {
             int j = i-1;
             k[j] = U * k[j-1] - alpha(j-1) * k[j-1];
-
-            // q_{n−1} U is a row times matrix. Since we store q as columns,
-            // we use U^T * q_{n−1} which corresponds to (q_{n−1} U)^T.
             q[j] = U.transpose() * q[j-1] - alpha(j-1) * q[j-1];
 
-            // β_{n−2} term is only present for i ≥ 3
             if (i >= 3) {
                 k[j] -= beta(j-2) * k[j-2];
                 q[j] -= beta(j-2) * q[j-2];
